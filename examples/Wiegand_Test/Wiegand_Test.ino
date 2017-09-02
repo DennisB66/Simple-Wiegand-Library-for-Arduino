4ef46b52124e0204737036d9faeb079c536360ff
DennisB66 <dennis.buis@home.nl>
2017-09-02 05:21:37 +0200

    formatting

--- examples/Wiegand_Test/Wiegand_Test.ino
+++ examples/Wiegand_Test/Wiegand_Test.ino
@@ -1,10 +1,10 @@
-// Copyright	: Dennis Buis (2017)
-// License  	: MIT
-// Platform		: Arduino
-// Library		:	Simple Wiegand Library for Arduino
-// File				: Wiegand_EEPROM_Test.ino
-// Purpose		: Example code for Wiegand library (see Wiegand.h)
-// Repository	:	https://github.com/DennisB66/Simple_Wiegand_Library_for_Arduino
+// Copyright  : Dennis Buis (2017)
+// License    : MIT
+// Platform   : Arduino
+// Library    : Simple Wiegand Library for Arduino
+// File       : Wiegand_EEPROM_Test.ino
+// Purpose    : Example code for Wiegand library (see Wiegand.h)
+// Repository : https://github.com/DennisB66/Simple-Wiegand-Library-for-Arduino
 
 #include <Wiegand.h>
 
@@ -14,13 +14,13 @@ void setup() {
   Serial.begin( 9600);
 
   Serial.println( "RFID reader initalizing... ");
-	wg.begin();
+  wg.begin();
 
   if ( wg.hasDevice()) {
-		Serial.println( "RFID reader ready");
-	} else {
-		Serial.println( "No RFID reader attached!");
-	}
+    Serial.println( "RFID reader ready");
+  } else {
+    Serial.println( "No RFID reader attached!");
+  }
 }
 
 void loop() {
